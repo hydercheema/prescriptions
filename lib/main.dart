3cd1409b1974c88963fa5e8b70e7831d0a4d7ebc
hyder cheema <hydercheema20@gmail.com>
2020-06-23 13:26:15 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,149 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(new MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return new MaterialApp(home: new MyHomePage());
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final GlobalKey<FormState> formkey = GlobalKey<FormState>();
+  String registeration, password;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return new Scaffold(
+
+        //main container
+        body: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("asset/background.jpg"),
+                fit: BoxFit.cover,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            child: new Form(
+                key: formkey,
+                child: ListView(children: <Widget>[
+                 SizedBox(height: 20,),
+                  Container(
+                    margin: const EdgeInsets.only(top: 100),
+                    child: Row(children: <Widget>[
+                      Spacer(),
+                     new ClipRRect(
+                          borderRadius: BorderRadius.circular(40.0),
+                          child: InkWell(
+                            onTap: (){print("2");},
+                              child: Image.asset("asset/doctor.jpeg",width: 80.0,height: 80.0,fit:BoxFit.fill))),
+                     new Spacer(),
+                     new ClipRRect(
+                          borderRadius: BorderRadius.circular(40.0),
+                          child: InkWell(
+                              onTap: (){print("1");},
+                              child: Image.asset("asset/paitent.jpeg",width: 80.0,height: 80.0,fit:BoxFit.fill))),
+                     new Spacer(),
+                     ],
+                    )
+                  ),
+                  new Row(
+                    children: <Widget>[
+                     new Spacer(),
+                      new Text("Doctor",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),),
+                     new Spacer(),
+                     new Text("Patient",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),),
+                     new Spacer()
+                    ],
+                  ),
+                  SizedBox(
+                    height: 60,
+                  ),
+
+                  new TextFormField(
+                    validator: (input) {
+                      if (input.isEmpty) {
+                        return 'Please enter your Registeration number';
+                      }
+                    },
+                    onSaved: (input) => registeration = input,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.perm_identity),
+                        labelText: "Registeration#",
+                        labelStyle: TextStyle(
+                            color: Colors.black38,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 20),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue, width: 5.0),
+                  ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue, width: 5.0),
+              ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+                  ),
+                  SizedBox(height: 30 ,),
+                  new TextFormField(
+                    validator: (input) {
+                      if (input.isEmpty) {
+                        return 'Please enter password';
+                      } else if (input.length < 6) {
+                        return 'Please enter password more than 6 characters';
+                      }
+                    },
+                    onSaved: (input) => password = input,
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.lock_outline),
+                        labelText: "Password",
+                        labelStyle: TextStyle(
+                            color: Colors.black38,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 20),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue, width: 5.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue, width: 5.0),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                  new Row (children: <Widget>[
+                    new Text("No Account"),
+                    new FlatButton(
+                      onPressed: null,
+                      color: Colors.blue,
+                      child: Text("Sign up",style: TextStyle(color: Colors.blue, fontSize: 15)
+                      ),
+                    ),
+                    Spacer(),
+                    new RaisedButton(
+                      color: Colors.blue,
+                        onPressed: (){},
+                            child:Text("Login",style: TextStyle(color: Colors.white),)
+                    ),
+                   ]
+                  ),
+                  Divider(
+                    color: Colors.black87,
+                    height: 30.0,
+                  ),
+                  
+                ]))));
   }
 }
